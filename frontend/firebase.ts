ca408cac0715b7288e9d6da8a63e1105ca640ae7
Erik H <urk@Eriks-MacBook-Pro.local>
2023-04-17 19:24:31 -0700

    updating firebase.ts to check if app is initialized before attempting to initialize

--- frontend/firebase.ts
+++ frontend/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -12,6 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
