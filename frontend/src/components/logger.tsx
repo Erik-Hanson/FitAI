a01cbeeb4c7967b8e6a4a2d86d2083eef2da7e6d
Davis Giang <47011080+giangdavis@users.noreply.github.com>
2023-04-18 17:20:35 -0700

    Chat history

--- frontend/src/components/logger.tsx
+++ frontend/src/components/logger.tsx
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import { Heading, Textarea, Button, VStack } from "@chakra-ui/react";
 import { useDocumentData } from "react-firebase-hooks/firestore";
-import { collection, doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  setDoc,
+  updateDoc,
+  arrayUnion,
+} from "firebase/firestore";
 import { firestore } from "../../firebase";
 
 interface User {
   sub: string;
+  name: string;
 }
 
 interface Entry {
   foodName: string;
   calories: number;
   servingSize: string;
+}
+
+interface Message {
+  sender: string;
+  text: string;
 }
 
 interface LoggerProps {
@@ -23,10 +35,17 @@ const Logger: React.FC<LoggerProps> = ({ user }) => {
 
   const currentDate = new Date().toISOString().slice(0, 10);
 
+  const userString = user ? `${user.name}-${user.sub}` : "";
   const foodDiariesRef =
-    user && collection(firestore, "users", user.sub, "foodDiaries");
+    user && collection(firestore, "users", userString!, "foodDiaries");
   const diaryDocRef = foodDiariesRef && doc(foodDiariesRef, currentDate);
+
+  const chatHistoryRef =
+    user && collection(firestore, "users", userString!, "chatHistory");
+  const chatDocRef = chatHistoryRef && doc(chatHistoryRef, currentDate);
+
   const [diaryData] = useDocumentData(diaryDocRef);
+  const [chatData] = useDocumentData(chatDocRef);
 
   const handleSubmit = async () => {
     if (user && mealDescription) {
@@ -35,11 +54,44 @@ const Logger: React.FC<LoggerProps> = ({ user }) => {
         calories: 0,
         servingSize: "unknown",
       }; // Replace with actual data fetched from the food API
-      const updatedEntries = diaryData?.entries
-        ? [...diaryData.entries, newEntry]
-        : [newEntry];
 
-      await setDoc(diaryDocRef, { date: currentDate, entries: updatedEntries });
+      if (!diaryData) {
+        await setDoc(diaryDocRef, {
+          date: currentDate,
+          entries: [newEntry],
+        });
+      } else {
+        const updatedEntries = [...diaryData.entries, newEntry];
+        await setDoc(diaryDocRef, {
+          date: currentDate,
+          entries: updatedEntries,
+        });
+      }
+
+      const userMessage: Message = {
+        sender: userString,
+        text: mealDescription,
+      };
+
+      if (!chatData) {
+        await setDoc(chatDocRef, {
+          date: currentDate,
+          messages: [userMessage],
+        });
+      } else {
+        await updateDoc(chatDocRef, {
+          messages: arrayUnion(userMessage),
+        });
+      }
+
+      // Add AI response
+      //   const aiMessage: Message = {
+      //     sender: "AI",
+      //     text: `${newEntry.foodName} - ${newEntry.calories} calories - ${newEntry.servingSize}`,
+      //   };
+      //   await updateDoc(chatDocRef, {
+      //     messages: arrayUnion(aiMessage),
+      //   });
 
       setMealDescription("");
     }
@@ -59,6 +111,12 @@ const Logger: React.FC<LoggerProps> = ({ user }) => {
           diaryData.entries.map((entry: Entry, index: number) => (
             <div key={index}>
               {entry.foodName} - {entry.calories} calories - {entry.servingSize}
+            </div>
+          ))}
+        {chatData?.messages &&
+          chatData.messages.map((message: Message, index: number) => (
+            <div key={index}>
+              {message.sender === userString ? "You" : "AI"}: {message.text}
             </div>
           ))}
       </VStack>
