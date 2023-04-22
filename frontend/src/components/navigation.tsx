5afc05fd60f86089b0282746dd45f85d843e6260
Davis Giang <47011080+giangdavis@users.noreply.github.com>
2023-04-22 15:55:01 -0700

    Fix: created interface for navItem children

--- frontend/src/components/navigation.tsx
+++ frontend/src/components/navigation.tsx
@@ -1,72 +1,64 @@
-import {
-    Box,
-    Button,
-    Flex,
-    Spacer,
-    useDisclosure,
-} from "@chakra-ui/react";
+import { Box, Button, Flex, Spacer, useDisclosure } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import Logout from "@/components/logout";
 import LoginButton from "@/components/login";
 
-const NavItem: React.FC = ({ children }: any) => {
-    return (
-        <Button
-            px={4}
-            py={2}
-            variant="ghost"
-            _hover={{ bg: "gray.700" }}
-            _focus={{ outline: "none" }}
-        >
-            {children}
-        </Button>
-    );
+interface NavItemProps {
+  children: React.ReactNode;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ children }) => {
+  return (
+    <Button
+      px={4}
+      py={2}
+      variant="ghost"
+      _hover={{ bg: "gray.700" }}
+      _focus={{ outline: "none" }}
+    >
+      {children}
+    </Button>
+  );
 };
 
 const Navigation: React.FC = (props: any) => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    return (
-        <Flex
-            as="nav"
-            align="center"
-            justify="space-between"
-            wrap="wrap"
-            p={4}
+  return (
+    <Flex as="nav" align="center" justify="space-between" wrap="wrap" p={4}>
+      {props.user ? (
+        <Box
+          display={{ base: isOpen ? "block" : "none", md: "flex" }}
+          width={{ base: "full", md: "auto" }}
+          alignItems="center"
+          flexGrow={1}
+          pl={4}
         >
-            {props.user ? (
-                <Box
-                    display={{ base: isOpen ? "block" : "none", md: "flex" }}
-                    width={{ base: "full", md: "auto" }}
-                    alignItems="center"
-                    flexGrow={1}
-                    pl={4}
-                >
-                    <NavItem>
-                        <Link href="/">Home</Link>
-                    </NavItem>
-                </Box>
-            ) : null }
+          <NavItem>
+            <Link href="/">Home</Link>
+          </NavItem>
+        </Box>
+      ) : null}
 
-            <Spacer />
+      <Spacer />
 
-            <Box
-                display={{ base: isOpen ? "block" : "none", md: "block" }}
-                mt={{ base: 4, md: 0 }}
-            >
-                {!props.user ? (
-                    <>
-                        {/* <Button variant="ghost" _hover={{ bg: "gray.700" }}>
+      <Box
+        display={{ base: isOpen ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}
+      >
+        {!props.user ? (
+          <>
+            {/* <Button variant="ghost" _hover={{ bg: "gray.700" }}>
                             <Link href="/login">Sign In/Sign Up</Link>
                         </Button> */}
-                        <LoginButton />
-                    </>
-                ) : null}
-                {props.user ? <Logout /> : null}
-            </Box>
-        </Flex>
-    );
+            <LoginButton />
+          </>
+        ) : null}
+        {props.user ? <Logout /> : null}
+      </Box>
+    </Flex>
+  );
 };
 
 export default Navigation;
